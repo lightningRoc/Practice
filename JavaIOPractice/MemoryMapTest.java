fea44875d372e8a397a5702d59970f08cd77c2f8
lightningRoc <1437527579@qq.com>
2019-02-08 21:02:39 +0800

    plus

---
+++ JavaIOPractice/MemoryMapTest.java
@@ -0,0 +1,119 @@
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.zip.CRC32;
+
+public class MemoryMapTest {
+	
+	//通过CRC32来操作文件来测试性能
+	public static long checksumInputStream(Path filename) throws IOException
+	{
+		//普通输入流测试
+		try(InputStream in=Files.newInputStream(filename))
+		{
+			CRC32 crc=new CRC32();
+			
+			int c;
+			while((c=in.read())!=-1)
+			{
+				crc.update(c);
+			}
+			return crc.getValue();
+		}
+	}
+	
+	public static long checksumBufferedInputStream(Path filename) throws IOException
+	{
+		//带缓冲的输入
+		try(InputStream in=new BufferedInputStream(Files.newInputStream(filename)))
+		{
+			CRC32 crc=new CRC32();
+			
+			int c;
+			while((c=in.read())!=-1)
+			{
+				crc.update(c);
+			}
+			return crc.getValue();
+		}
+	}
+	
+	public static long checksumRandomAccessFile(Path filename) throws FileNotFoundException, IOException
+	{
+		//文件随机访问
+		try(RandomAccessFile file=new RandomAccessFile(filename.toFile(),"r"))
+		{
+			long length=file.length();
+			CRC32 crc=new CRC32();
+			
+			for(long p=0;p<length;p++)
+			{
+				file.seek(p);
+				int c=file.readByte();
+				crc.update(c);
+			}
+			return crc.getValue();
+		}
+	}
+	
+	public static long checksumMappedFile(Path filename) throws IOException
+	{
+		//内存映射方式访问
+		try(FileChannel channel=FileChannel.open(filename))
+		{
+			CRC32  crc=new CRC32();
+			int length=(int)channel.size();
+			MappedByteBuffer buffer=channel.map(FileChannel.MapMode.READ_ONLY, 0, length);
+			
+			for(int p=0;p<length;p++)
+			{
+				int c=buffer.get(p);
+				crc.update(c);
+			}
+			return crc.getValue();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		Path filename=Paths.get(scan.next());
+				
+		System.out.println("普通输入流:");
+		long start=System.currentTimeMillis();
+		long crcValue=checksumInputStream(filename);
+		long end=System.currentTimeMillis();
+		System.out.println(Long.toHexString(crcValue));
+		System.out.println((end-start)+"ms");
+		
+		System.out.println("带缓冲输入流:");
+		start=System.currentTimeMillis();
+		crcValue=checksumBufferedInputStream(filename);
+		end=System.currentTimeMillis();
+		System.out.println(Long.toHexString(crcValue));
+		System.out.println((end-start)+"ms");
+		
+		System.out.println("随机访问文件:");
+		start=System.currentTimeMillis();
+		crcValue=checksumRandomAccessFile(filename);
+		end=System.currentTimeMillis();
+		System.out.println(Long.toHexString(crcValue));
+		System.out.println((end-start)+"ms");
+		
+		System.out.println("内存映射文件s:");
+		start=System.currentTimeMillis();
+		crcValue=checksumMappedFile(filename);
+		end=System.currentTimeMillis();
+		System.out.println(Long.toHexString(crcValue));
+		System.out.println((end-start)+"ms");
+	}
+
+}
